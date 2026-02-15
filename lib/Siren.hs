64c67a835025c35520e482dc5d815c8939fea7a5
Marco Marinho <marcoobom@gmail.com>
2026-02-15 19:12:04 +0100

    Stuff

--- lib/Siren.hs
+++ lib/Siren.hs
@@ -39,6 +39,31 @@ doTrippleSwap freqs swaps target = runST $ do
   swap swaps
   MV.read frequencies target
 
+doSwapBlock :: [Int] -> [(Int, Int)] -> Int -> Int
+doSwapBlock freqs swaps target = runST $ do
+  frequencies <- V.thaw (V.fromList freqs)
+  let numFrequencies = MV.length frequencies
+
+  let swapBlock x y left = do
+        case left of
+          0 -> return ()
+          _ -> do
+            aFreq <- MV.read frequencies x
+            bFreq <- MV.read frequencies y
+            MV.write frequencies x bFreq
+            MV.write frequencies y aFreq
+            swapBlock (x+1) (y+1) (left - 1)
+  
+  let swap swapList = do
+        case swapList of
+          [] -> return ()
+          (a, b) : tl -> do
+            let swapLen = minimum [abs(a - b), numFrequencies - a, numFrequencies - b]
+            swapBlock a b swapLen
+            swap tl
+  swap swaps
+  MV.read frequencies target
+
 parseSwap :: String -> (Int, Int)
 parseSwap line = case splitOn "-" line of
   [a, b] -> (read a - 1, read b - 1)
@@ -84,8 +109,24 @@ part2 input = show result
       _ -> error "Invalid target format"
     result = doTrippleSwap freqs swaps target
 
+part3 :: String -> String
+part3 input = show result
+  where
+    iLines = lines input
+    blocks = splitOn [""] iLines
+    (freqsStr, swapsStr, targetStr) = case blocks of
+      [f, s, t] -> (f, s, t)
+      _ -> error "Invalid input format"
+    freqs = map read freqsStr :: [Int]
+    swaps = map parseSwap swapsStr
+    target = case targetStr of
+      [t] -> read t - 1 :: Int
+      _ -> error "Invalid target format"
+    result = doSwapBlock freqs swaps target
+
 solve :: Int -> String -> String
 solve n input = case n of
   1 -> part1 input
   2 -> part2 input
+  3 -> part3 input
   _ -> error "Invalid part number"
